6e4c6b34fe31ddee0cb8da4f341bb2b5938617a7
Arthur F Heezen III <arthurheezen@gmail.com>
2016-07-20 12:33:46 -0500

    Fix Fund 0013 - no other section L1 headers after LIABILITIES within same fund

--- SQLScripts/CREATE TABLE A2SectionL1Limits.sql
+++ SQLScripts/CREATE TABLE A2SectionL1Limits.sql
@@ -1,63 +1,81 @@
 CREATE TABLE
   A2SectionL1Limits
-AS SELECT 
-  SectionL1Start,
-  SectionL1End,
-  SectionL1Code
-FROM   
-  A2SectionL1LimitsTemp
-WHERE
- SectionL1Start<>0
- AND SectionL1End<>0
-
-UNION ALL
-
-SELECT 
-  MAX(AllEnds.SectionL1End)+10801 AS SectionL1Start,
-  NoSectionStart.SectionL1End, 
-  NoSectionStart.SectionL1Code
-FROM (
-    SELECT 
-      SectionL1Code,
-      SectionL1End
-    FROM   
-      A2SectionL1LimitsTemp
-    WHERE
-     SectionL1Start=0
-  ) NoSectionStart
-INNER JOIN (
-    SELECT 
-      SectionL1End 
-    FROM   
-      A2SectionL1LimitsTemp
-  ) AllEnds
-ON AllEnds.SectionL1End < NoSectionStart.SectionL1End
-GROUP BY
-  NoSectionStart.SectionL1Code,
-  NoSectionStart.SectionL1End
-
-UNION ALL
-
-SELECT 
-  NoSectionEnd.SectionL1Start AS SectionL1Start, 
-  MIN(AllStarts.SectionL1Start)-10801 AS SectionL1End,
-  NoSectionEnd.SectionL1Code AS SectionL1Code
-FROM (
-    SELECT 
-      SectionL1Code,
-      SectionL1Start
-    FROM   
-      A2SectionL1LimitsTemp
-    WHERE
-     SectionL1End=0
-  ) NoSectionEnd
-INNER JOIN (
-    SELECT 
-      SectionL1Start 
-    FROM   
-      A2SectionL1LimitsTemp
-  ) AllStarts
-ON NoSectionEnd.SectionL1Start < AllStarts.SectionL1Start
-GROUP BY
-  NoSectionEnd.SectionL1Code,
-  NoSectionEnd.SectionL1Start
+AS 
+  
+  -- Start with L1 limits that are balanced (header and total are both present)
+  SELECT 
+    SectionL1Start,
+    SectionL1End,
+    SectionL1Code
+  FROM   
+    A2SectionL1LimitsTemp
+  WHERE
+   SectionL1Start<>0
+   AND SectionL1End<>0
+  
+  UNION ALL
+  
+  -- Union L1 limits with no header/start
+  SELECT 
+    MAX(AllEnds.SectionL1End)+10801 AS SectionL1Start,
+    NoSectionStart.SectionL1End, 
+    NoSectionStart.SectionL1Code
+  FROM (
+      SELECT 
+        SectionL1Code,
+        SectionL1End
+      FROM   
+        A2SectionL1LimitsTemp
+      WHERE
+       SectionL1Start=0
+    ) NoSectionStart
+  INNER JOIN (
+      SELECT 
+        SectionL1End 
+      FROM   
+        A2SectionL1LimitsTemp
+    ) AllEnds
+  ON AllEnds.SectionL1End < NoSectionStart.SectionL1End
+  GROUP BY
+    NoSectionStart.SectionL1Code,
+    NoSectionStart.SectionL1End
+  
+  UNION ALL
+  
+  -- Union L1 limits with no total/end
+  SELECT 
+    NoSectionEnd.SectionL1Start AS SectionL1Start, 
+    MIN(AllStartsUnionFundTotals.SectionL1Start)-10801 AS SectionL1End,
+    NoSectionEnd.SectionL1Code AS SectionL1Code
+  FROM (
+      SELECT 
+        SectionL1Code,
+        SectionL1Start
+      FROM   
+        A2SectionL1LimitsTemp
+      WHERE
+       SectionL1End=0
+    ) NoSectionEnd
+    
+  INNER JOIN (
+      
+      -- Starts of new sections
+      SELECT 
+        SectionL1Start 
+      FROM   
+        A2SectionL1LimitsTemp
+      
+      UNION ALL
+      
+      -- End of funds
+      SELECT
+        FundEnd AS SectionL1Start
+      FROM
+        A2FundLimits
+        
+    ) AllStartsUnionFundTotals
+    
+  ON NoSectionEnd.SectionL1Start < AllStartsUnionFundTotals.SectionL1Start
+  GROUP BY
+    NoSectionEnd.SectionL1Code,
+    NoSectionEnd.SectionL1Start
